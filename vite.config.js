9e76219f0d623a74652c03c815000cc37553c8b2
Himanshu Sharma <sharmaslov@gmail.com>
2025-02-15 14:14:20 +0530

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://himuexe.github.io/e-plantShopping/",
+  base: "/e-plantShopping/",
   homepage: "https://himuexe.github.io/e-plantShopping/",
   plugins: [react()],
 })
